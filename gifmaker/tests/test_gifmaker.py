1a7b00ccbe89b275b1b418627f07207cc80184b0
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-06-08 21:08:38 -0400

    Fixed path for tests

--- gifmaker/tests/test_gifmaker.py
+++ gifmaker/tests/test_gifmaker.py
@@ -2,13 +2,17 @@
 # -*- coding: utf-8
 # pytest unit tests for gifmaker
 
+import os
 import imageio
 
+from gifmaker import __path__
 from gifmaker.gifmaker import *
 
 
 def test_creategif():
     """Very basic integrity test that checks if an output gif with proper dimensions is created"""
-    creategif(['images/1.png', 'images/2.png'], 'anim.gif', 0.1)
+    creategif([os.path.join([__path__, 'tests/images/1.png']),
+               os.path.join([__path__, 'tests/images/2.png'])],
+              'anim.gif', 0.1)
     im = imageio.imread('anim.gif')
     assert im.shape == (1344, 2294, 4)
