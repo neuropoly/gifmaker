252ebd070d6bd92f45044fbaecae4b9cea9376d6
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-06-08 21:14:11 -0400

    Fixed path issue in tests

--- gifmaker/tests/test_gifmaker.py
+++ gifmaker/tests/test_gifmaker.py
@@ -10,9 +10,9 @@ from gifmaker.gifmaker import *
 
 
 def test_creategif():
-    """Very basic integrity test that checks if an output gif with proper dimensions is created"""
-    creategif([os.path.join([__path__, 'tests/images/1.png']),
-               os.path.join([__path__, 'tests/images/2.png'])],
+    """Very basic integrity test that checks if a gif is created, and with proper dimensions."""
+    creategif([os.path.join(__path__[0], 'tests/images/1.png'),
+               os.path.join(__path__[0], 'tests/images/2.png')],
               'anim.gif', 0.1)
     im = imageio.imread('anim.gif')
     assert im.shape == (1344, 2294, 4)
