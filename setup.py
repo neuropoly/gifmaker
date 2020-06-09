e2a603d0b69f34b3fe1be387fb127c9a0b951a46
Julien Cohen-Adad <jcohen@polymtl.ca>
2020-06-08 23:24:49 -0400

    Fixed markdown flag missing in setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,9 @@ import gifmaker as gm
 with open('requirements.txt') as f:
     requirements = f.readlines()
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='gifmaker',
     version=gm.__version__,
@@ -14,11 +17,18 @@ setup(
     url='',
     license='LICENSE',
     description='Convert a series of images into a gif animation.',
-    long_description=open('README.md').read(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     install_requires=requirements,
     entry_points={
         'console_scripts': [
             'gifmaker = gifmaker.gifmaker:main',
             ]
-        }
+        },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        ],
+    python_requires='>=3.5',
     )
